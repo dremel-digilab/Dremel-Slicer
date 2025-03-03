42fe76bc931d37fd7e76a573efba1320726f1efd
Christian Delprete <hello@bitprism.co>
2025-03-02 18:14:12 -0600

    Misc QA.

--- build_release_vs2022.bat
+++ build_release_vs2022.bat
@@ -52,7 +52,7 @@ cmake --build . --config %build_type% --target deps -- -m
 if "%1"=="deps" exit /b 0
 
 :slicer
-echo "building Dremel Slicer..."
+echo "building Dremel 3D Slicer..."
 cd %WP%
 mkdir %build_dir%
 cd %build_dir%
