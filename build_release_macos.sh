b531d99756df7925f51b3fbb46e6fb9346dacbb8
Christian Delprete <hello@bitprism.co>
2025-03-24 22:08:32 -0500

    Testimg mac-15 runner.

--- build_release_macos.sh
+++ build_release_macos.sh
@@ -134,7 +134,6 @@ function build_deps() {
                 -DCMAKE_OSX_ARCHITECTURES:STRING="${ARCH}" \
                 -DCMAKE_OSX_DEPLOYMENT_TARGET="${OSX_DEPLOYMENT_TARGET}"
         fi
-        echo "Manually building GLEW with verbose output for diagnostics..."
         cmake --build . --config "$BUILD_CONFIG" --target deps
     )
 }
